5a17ab06abc2778960fc03df00d1ee4fb6ab14da
harani-36 <kkanumaharani@gmail.com>
2025-01-14 18:57:54 +0530

    Create Top k frequent words.java

---
+++ walmart/Top k frequent words.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        TreeMap<String , Integer> map = new TreeMap<>();
+        for(String word:words){
+            map.put(word , map.getOrDefault(word, 0)+1);
+        }
+        
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int n:map.values()){
+            maxheap.add(n);
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1;i<=k;++i){
+            int n = maxheap.poll();
+            String str = findword(map , n);
+            map.remove(str);
+            list.add(str);
+        }
+        return list;
+    }
+    public String findword(TreeMap<String ,Integer> map , int n){
+        for(String s:map.keySet()){
+            if(map.get(s)==n){
+                return s;
+            }
+        }
+        return " ";
+    }
+}
