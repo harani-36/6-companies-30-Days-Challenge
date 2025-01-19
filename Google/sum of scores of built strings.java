696657dbb70aec807896e2d6d0ab0f16d37f87ea
harani-36 <kkanumaharani@gmail.com>
2025-01-19 19:41:21 +0530

    Create sum of scores of built strings.java

---
+++ Google/sum of scores of built strings.java
@@ -0,0 +1,37 @@
+class Solution {
+   public long sumScores(String s) {
+        int size = s.length();
+        int[] zArr = getZArray(s);
+        long ans = 0;
+        for(int i = size+1; i<zArr.length; i++)
+            ans +=zArr[i];
+        return ans;
+    }
+    int[] getZArray(String s){
+        int l = 0, r = 0;
+        s= s+"$"+s;
+        int[] zArr = new int[s.length()];
+        for(int i =1; i<s.length(); i++){
+            if(i>r){
+                l = r = i;
+                while(r<s.length() && s.charAt(r) == s.charAt(r-l))
+                    r++;
+                zArr[i] = r-l;
+                r--;
+            }
+            else{
+                int k = i-l;
+                if(zArr[k]<r-i+1)
+                    zArr[i] = zArr[k];
+                else{
+                    l = i;
+                    while(r<s.length() && s.charAt(r) == s.charAt(r-l))
+                        r++;
+                    zArr[i] = r-l;
+                    r--;
+                }
+            }
+        }
+        return zArr;
+    }
+}
